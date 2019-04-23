012c11a4176728f9be87505912596a70e0bf5220
XinLiu92 <xliu0410@gmail.com>
2019-04-22 20:05:48 -0400

    add query expansion method by wordnet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import edu.unh.cs.treccar_v2.Data;
 import main.java.QueryExpansion.QueryExpansion;
 import main.java.QueryExpansion.QueryExpansionLDA;
 import main.java.QueryExpansion.QueryExpansionQueryEntity;
+import main.java.QueryExpansion.QueryExpansionWordNet;
 import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.io.File;
@@ -16,7 +17,7 @@ import java.util.Map;
 
 public class Main {
     static private String INDEX_DIRECTORY = "/Users/xin/Documents/19Spring/DS/index";
-    static private String OUTPUT_DIR = "out";
+    static private String OUTPUT_DIR = "output";
     static final private int Max_Results = 100;
 
     static IndexData indexer;
@@ -24,8 +25,8 @@ public class Main {
     public static void main(String[] args) throws Exception,IOException, ParseException {
         System.setProperty("file.encoding", "UTF-8");
 
-        String queryPath = "/Users/xin/Desktop/benchmarkY2.public/benchmarkY2.cbor-outlines.cbor";
-
+        //String queryPath = "/Users/xin/Desktop/benchmarkY2.public/benchmarkY2.cbor-outlines.cbor";
+        String queryPath = "/Users/xin/Documents/19Spring/DS/benchmarkY1/benchmarkY1-train/train.pages.cbor-outlines.cbor";
 
         String dataPath = "/Users/xin/Documents/19Spring/DS/test200/test200-train/train.pages.cbor-paragraphs.cbor";
 
@@ -33,21 +34,23 @@ public class Main {
 
 
         INDEX_DIRECTORY = args[0];
-        //queryPath = args[1];
-        dataPath = args[1];
+        queryPath = args[1];
+        //dataPath = args[1];
         OUTPUT_DIR = args[2];
 
-        indexer = new IndexData(INDEX_DIRECTORY, dataPath);
-        indexer.reIndex();
+        //indexer = new IndexData(INDEX_DIRECTORY, dataPath);
+        //indexer.reIndex();
 
-//        QueryData queryData = new QueryData(queryPath);
+        QueryData queryData = new QueryData(queryPath);
 //
-//        Map<String,String> pageMap = queryData.getAllPageQueries();
-//        Map<String,String> sectionMap = queryData.getAllSectionQueries();
-//        ArrayList<Data.Page> pageList = queryData.getPageList();
-//        ArrayList<Data.Section> sectionList = queryData.getSectionList();
-//
-//       //  Store all query strings temporarily.
+        Map<String,String> pageMap = queryData.getAllPageQueries();
+        Map<String,String> sectionMap = queryData.getAllSectionQueries();
+        ArrayList<Data.Page> pageList = queryData.getPageList();
+        ArrayList<Data.Section> sectionList = queryData.getSectionList();
+
+        QueryExpansionWordNet queryExpansionWordNet = new QueryExpansionWordNet(pageMap,sectionMap,INDEX_DIRECTORY,OUTPUT_DIR);
+        queryExpansionWordNet.runPage();
+        //       //  Store all query strings temporarily.
 //
 //
 //        System.out.println("Got " + pageMap.size() + " pages and " + sectionMap.size() + " sections.");
@@ -84,7 +87,7 @@ public class Main {
 //        BL section_bl = new BL(INDEX_DIRECTORY, Max_Results, OUTPUT_DIR);
 //        section_bl.RankDocWithBigram_Laplace(sectionMap, OUTPUT_DIR+"/"+"BigramLanguageModel-Laplace-Section.run");
 
-        System.out.println("QueryExpansion Begin");
+//        System.out.println("QueryExpansion Begin");
 //        QueryExpansion qe = new QueryExpansion(pageMap,sectionMap,INDEX_DIRECTORY,OUTPUT_DIR);
 //
 //        qe.runPage();
---
+++ src/main/java/QueryExpansion/QueryExpansionWordNet.java
@@ -0,0 +1,178 @@
+package main.java.QueryExpansion;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import rita.*;
+import rita.wordnet.*;
+import net.sf.extjwnl.*;
+import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.data.Synset;
+import net.sf.extjwnl.dictionary.Dictionary;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.*;
+import java.util.*;
+
+public class QueryExpansionWordNet {
+    private IndexSearcher searcher;
+    private Map<String,String> pageMap;
+    private Map<String,String> sectionMap;
+    private String INDEX_DIR;
+    private int max_result;
+    private QueryParser parser;
+    static  private String OUTPUT_DIR="output";
+    private Dictionary wnDict = null;
+    private String propsFile = "";
+    private Set<String> stopWordsSet;
+
+    public QueryExpansionWordNet(Map<String, String> pageMap,Map<String,String> sectionMap, String indexPath,String output) throws JWNLException, FileNotFoundException {
+        this.pageMap = pageMap;
+        this.INDEX_DIR = indexPath;
+        this.max_result = 100;
+        this.sectionMap = sectionMap;
+        OUTPUT_DIR = output;
+        this.stopWordsSet = new HashSet<>();
+        wnDict = Dictionary.getDefaultResourceInstance();
+
+
+        List<String> list = new ArrayList<>();
+
+        String line = "";
+        String stopWordDir = "src/resources/stop_word.cfg";
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(stopWordDir));
+
+            while ((line = bufferedReader.readLine()) != null){
+                if (!line.isEmpty()){
+//                    list.add(line.replace(" ",""));
+                    stopWordsSet.add(line.replace(" ",""));
+                }
+            }
+
+            bufferedReader.close();
+
+//            CharArraySet stopWord = new CharArraySet(list,true);
+
+
+//            System.out.println(stopWordsSet.size());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void runPage() throws IOException, ParseException {
+        try {
+            run(pageMap,"page-QueryExpansionWordNet.run");
+        } catch (JWNLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void runSection() throws IOException,ParseException{
+        try {
+            run(sectionMap,"section-QueryExpansionWordNet.run");
+        } catch (JWNLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getExpandedQuery(String queryStr){
+        RiWordNet wordNet = new RiWordNet("/usr/local/WordNet-3.0");
+        wordNet.randomizeResults(false);
+        StringBuilder sb = new StringBuilder();
+
+        String[] splitStr = queryStr.split(" ");
+
+        for (String str : splitStr){
+            if (stopWordsSet.contains(str)) continue;
+            String pos = wordNet.getBestPos(str);
+//                System.out.println(pos+" "+str);
+            if (pos == null) {
+                sb.append(str);
+                sb.append(" ");
+                continue;
+            }
+            String[] s = wordNet.getAllSynsets(str,pos);
+//                System.out.println(Arrays.asList(s));
+            if (s.length == 0){
+                sb.append(str);
+            }else{
+                sb.append(s[0].toLowerCase());
+            }
+//
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public void run(Map<String,String> map,String fileName) throws IOException, ParseException, JWNLException {
+        searcher = new IndexSearcher(DirectoryReader.open(FSDirectory.open((new File(INDEX_DIR).toPath()))));
+
+        searcher.setSimilarity(new BM25Similarity());
+
+        parser = new QueryParser("content", new StandardAnalyzer());
+
+
+        Set<String> runFileStr = new HashSet<>();
+
+        for (Map.Entry<String, String> entry:map.entrySet()){
+            String queryStr = entry.getValue();
+            String queryId = entry.getKey();
+
+
+            String newQuery = getExpandedQuery(queryStr);
+
+            Query q = parser.parse(QueryParser.escape(newQuery));
+
+            TopDocs tops = searcher.search(q, max_result);
+            ScoreDoc[] scoreDoc = tops.scoreDocs;
+
+
+            for (int i = 0; i < scoreDoc.length;i++ ){
+                ScoreDoc score = scoreDoc[i];
+                Document doc = searcher.doc(score.doc);
+
+                String paraId = doc.getField("paraid").stringValue();
+                float rankScore = score.score;
+                int rank = i+1;
+                String runStr = queryId+" Q0 "+paraId+" "+rank+ " "+rankScore+" "+"team3"+" QueryExpansionWordNet";
+
+                if (!runFileStr.contains(runStr)){
+                    runFileStr.add(runStr);
+                }
+            }
+        }
+
+        writeToFile(fileName,runFileStr);
+
+
+    }
+
+    private void writeToFile(String filename, Set<String> runfileStrings) {
+        String fullpath = OUTPUT_DIR + "/" + filename;
+
+        try (FileWriter runfile = new FileWriter(new File(fullpath))) {
+            for (String line : runfileStrings) {
+                runfile.write(line + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Could not open " + fullpath);
+        }
+
+        System.out.println("wrote file to "+ OUTPUT_DIR);
+    }
+
+
+
+}
